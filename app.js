b32cf31ca7bf7f0edf139ed14480c43ba56b0ea3
Jasmin I <59654502+JsmnIlj@users.noreply.github.com>
2021-04-20 14:37:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+        behavior: 'smooth'});
     });
-});
 });
