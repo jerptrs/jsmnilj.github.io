4f1e9cc0ca02400bf6d26657fdb525f96661184f
jerptrs <jerptrs@gmail.com>
2021-05-07 18:05:22 +0200

    formatted javascript

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-$(window).scroll(function() {
-    parallax();
-})
+$(window).scroll(function () {
+  parallax();
+});
 
 function parallax() {
+  var wScroll = $(window).scrollTop();
 
-    var wScroll = $(window).scrollTop();
-    
-    $('.parallax--bg').css('background-position', `center ${wScroll*0.75}px`)
+  $(".parallax--bg").css("background-position", `center ${wScroll * 0.75}px`);
 
-    $('.parallax--box').css('top', `${-5 + (wScroll * 0.005)}em`)
+  $(".parallax--box").css("top", `${-5 + wScroll * 0.005}em`);
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'});
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
